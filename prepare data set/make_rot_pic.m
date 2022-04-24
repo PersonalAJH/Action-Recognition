f4a28d6561d031ee39a49ac72bebaef8ea6727a2
Junghwan <personaljh3@gmail.com>
2022-04-24 14:41:32 +0900

    first commit/ 태권도 동작인식 CNN 아키텍쳐 및 Xsens data set 준비를 위한 코드(MatLab)

---
+++ prepare data set/make_rot_pic.m
@@ -0,0 +1,13 @@
+function trash = make_rot_pic(data,frame_start,frame_end,i,j)
+
+    pic(:,:,1) = data(frame_start:frame_end,2:70) - data(frame_start,2:70);
+    pic(:,:,2) = data(frame_start:frame_end,282:350);
+    pic(:,:,3) = data(frame_start:frame_end,352:420);
+    
+    lists = dir(cd);
+    [m,n] = size(lists);
+    img_name = m-1;
+    imwrite(pic, sprintf('%d-%d-%d.png',img_name,i,j));
+    img_name = img_name + 1;
+
+end
